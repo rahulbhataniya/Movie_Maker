dd0b6285ae2201377f7d3a6bbb4b34f24279c822
rahul_bhataniya <rahulbhataniya2468@gmail.com>
2023-05-16 09:27:59 +0530

    Modify the split_on_silence file of pydub.silence to get the start and end time of each chunk

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Movie_Maker
+
+Packages :
+
+1. pip install speechrecognition
+
+Sources :
+
+convert speech to text :
+
+1. https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
+
+2. https://realpython.com/python-speech-recognition/
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,18 +1,65 @@
+# importing libraries
 import speech_recognition as sr
+import os
+from pydub import AudioSegment
+import silence_library_copy as split_on_silence
 
-# create a recognizer object
+# create a speech recognition object
 r = sr.Recognizer()
 
-# specify the audio file path
-audio_file = "path/to/audio/file.wav"
+# a function to recognize speech in the audio file
+# so that we don't repeat ourselves in in other functions
 
-# use the recognizer to open the audio file
-with sr.AudioFile(audio_file) as source:
-    # listen for the data (load audio to memory)
-    audio_data = r.record(source)
 
-# transcribe the audio data
-text = r.recognize_google(audio_data)
+def transcribe_audio(path):
+    # use the audio file as the audio source
+    with sr.AudioFile(path) as source:
+        audio_listened = r.record(source)
+        # try converting it to text
+        text = r.recognize_google(audio_listened)
+    return text
 
-# print the transcribed text
-print(text)
+# a function that splits the audio file into chunks on silence
+# and applies speech recognition
+
+
+def get_large_audio_transcription_on_silence(path):
+    """Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks"""
+    # open the audio file using pydub
+    sound = AudioSegment.from_file(path)
+    # split audio sound where silence is 500 miliseconds or more and get chunks
+    start_end_time,chunks = split_on_silence.split_on_silence(sound,
+                              # experiment with this value for your target audio file
+                              min_silence_len=100,
+                              # adjust this per requirement
+                              silence_thresh=sound.dBFS-14,
+                              # keep the silence for 1 second, adjustable as well
+                              keep_silence=500,
+                              )
+    print(start_end_time)
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        try:
+            text = transcribe_audio(chunk_filename)
+        except sr.UnknownValueError as e:
+            print("Error:", str(e))
+        else:
+            text = f"{text.capitalize()}. "
+            print(chunk_filename, ":", text)
+            whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+
+print(get_large_audio_transcription_on_silence('static/output/audio.wav'))
